9d8bd0895e6a7ed85f90869d2d67d5a4e831a69a
Zeppelin_K <littlejun3004@outlook.com>
2024-10-24 19:12:27 +0800

    Updatedd BEV

--- BEV/Advanced-Lane-Line-Detection/perspective.py
+++ BEV/Advanced-Lane-Line-Detection/perspective.py
@@ -1,7 +1,3 @@
-#This cod gets perspective transform of a image using 
-#predefined parameters.
-
-IMG_PATH = "E:\KazeRacing\BEV\Advanced-Lane-Line-Detection\\test_images\\Standard.png"
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import matplotlib.gridspec as gridspec
@@ -9,108 +5,40 @@ from PIL import Image
 import cv2
 import numpy as np
 import pickle as pickle
-import glob
+import os
 import settings
 
-roi_points = np.array([[0, settings.ORIGINAL_SIZE[1] - 50],
-                     [settings.ORIGINAL_SIZE[0], settings.ORIGINAL_SIZE[1] - 50],
-                     [settings.ORIGINAL_SIZE[0]//2, settings.ORIGINAL_SIZE[1]//2+50]], np.int32)
-print('roi_points: ', roi_points)
-# Find the region of interest
-roi = np.zeros((720, 1280), np.uint8)
-print('roi: ', roi.shape)
-cv2.fillPoly(roi, [roi_points], 1)
 
-#--------  Apply Processing to example  ----------------
-img = np.array(Image.open(IMG_PATH))
 
-# convert to hls
-img_hs1 = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-
-# Canny
-# Use the lightness layer to detect lines
-low_thresh = 100
-high_thresh = 150
-
-# Lightness thresholding (returns less points than saturation thresholding, gives better representation of lane lines)
-edges_lightness = cv2.Canny(img_hs1[:,:,1], high_thresh, low_thresh)
-
-#Line Detection
-lines = cv2.HoughLinesP(edges_lightness, 0.5, np.pi/180, 20, None, 180, 120)
-
-Lhs = np.zeros((2, 2), dtype = np.float32)
-Rhs = np.zeros((2, 1), dtype = np.float32)
-x_max = 0
-x_min = 2555
-for line in lines:
-    for x1, y1, x2, y2 in line:
-        # Find the norm (the distances between the two points)
-        normal = np.array([[-(y2-y1)], [x2-x1]], dtype = np.float32) # question about this implementation
-        normal = normal / np.linalg.norm(normal)
-        
-        pt = np.array([[x1], [y1]], dtype = np.float32)
-        
-        outer = np.matmul(normal, normal.T)
-        
-        Lhs += outer
-        Rhs += np.matmul(outer, pt) #use matmul for matrix multiply and not dot product
-
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), thickness = 1)
-        
-        x_iter_max = max(x1, x2)
-        x_iter_min = min(x1, x2)
-        x_max = max(x_max, x_iter_max)
-        x_min = min(x_min, x_iter_min)
-
-width = x_max - x_min
-print('width : ', width)
-
-# Calculate Vanishing Point
-vp = np.matmul(np.linalg.inv(Lhs), Rhs)
-
-# Find the transform matrix
-def find_pt_inline(p1, p2, y):
-    """
-    Here we use point-slope formula in order to find a point that is present on the line
-    that passes through our vanishing point (vp). 
-    input: points p1, p2, and y. They come is as tuples [x, y]
-    We then use the point-slope formula: y - b = m(x - a)
-    y: y-coordinate of desired point on the line
-    x: x-coordinate of desired point on the line
-    m: slope
-    b: y-coordinate of p1
-    a: x-coodrinate of p1
-    x = p1x + (1/m)(y - p1y)
-    """
-    m_inv = (p2[0] - p1[0]) / float(p2[1] - p1[1])
-    Δy = (y - p1[1])
-    x = p1[0] + m_inv * Δy
-    return [x, y]
-
-top = vp[1] + 65
-bot = settings.ORIGINAL_SIZE[1] -210
-
-# Make a large width so that you can grab the lines on the challenge video
-width = 500
-center = vp[0]
-p1 = [center - width/2, top]
-p2 = [center + width/2, top]
-p3 = find_pt_inline(p2, vp, bot)
-p4 = find_pt_inline(p1, vp, bot)
-
-D = [0, 0]
-dst_pts = np.float32([[0, 0], [settings.UNWARPED_SIZE[0], 0],
+def transform_image(img,filename):
+    dst_pts = np.float32([[0, 0], [settings.UNWARPED_SIZE[0], 0],
                        [settings.UNWARPED_SIZE[0], settings.UNWARPED_SIZE[1]],
                        [0, settings.UNWARPED_SIZE[1]]])
-A = p1[0][0]
-src_pts = np.float32([[p1[0][0],p1[1][0]],
-                      [p2[0][0],p2[1][0]],
-                      [p3[0][0],p3[1]],
-                      [p4[0][0],p4[1]]])
 
+    src_pts = np.float32([[287.64636,236.55095],
+                      [987.64636,236.55095],
+                      [1588.3408,420],
+                      [-251.8982,420]])
+    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+    warped = cv2.warpPerspective(np.array(Image.open(img)), M, settings.UNWARPED_SIZE)
+    cv2.imwrite('output_images/'+filename+'_warped.png', cv2.cvtColor(warped, cv2.COLOR_RGB2BGR))
+    
 
-# H is the homography matrix
-M_inv = cv2.getPerspectiveTransform(dst_pts, src_pts)
-M = cv2.getPerspectiveTransform(src_pts, dst_pts)
-warped = cv2.warpPerspective(np.array(Image.open(IMG_PATH)), M, settings.UNWARPED_SIZE)
-cv2.imwrite('./warped.png', cv2.cvtColor(warped, cv2.COLOR_RGB2BGR))
+def process_images(directory):
+    # 确保输出文件夹存在
+    output_folder = 'output_images'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    # 遍历目录中的所有文件
+    for filename in os.listdir(directory):
+        if filename.endswith('.png'):
+            # 构建完整的文件路径
+            base_filename = os.path.splitext(filename)[0]
+            file_path = os.path.join(directory, filename)
+            # 调用transform_image方法处理图像
+            transform_image(file_path, base_filename)
+if __name__ == '__main__':
+    # 指定你的图片目录
+    image_directory = r"E:\KazeRacing\Recorded Track\f1_large_TapeA"
+    process_images(image_directory)
--- BEV/BEV_cv2.py
+++ BEV/BEV_cv2.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 
 # Load the image
 img = cv2.imread('Standard.png') 
@@ -19,13 +20,14 @@ plt.show()"""
 IMAGE_H = 720
 IMAGE_W = 1280
 
-src = np.float32([[0, IMAGE_H], [1207, IMAGE_H], [0, 0], [IMAGE_W, 0]])
-dst = np.float32([[569, IMAGE_H], [711, IMAGE_H], [0, 0], [IMAGE_W, 0]])
+src = np.float32([[0, IMAGE_H], [1280, IMAGE_H], [0, 200], [IMAGE_W, 200]])
+dst = np.float32([[500, IMAGE_H], [700, IMAGE_H], [0, 0], [IMAGE_W, 0]])
 M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
 Minv = cv2.getPerspectiveTransform(dst, src) # Inverse transformation
 
-img = cv2.imread('./test_img.jpg') # Read the test img
-img = img[450:(450+IMAGE_H), 0:IMAGE_W] # Apply np slicing for ROI crop
+img = np.array(Image.open('E:\KazeRacing\BEV\Advanced-Lane-Line-Detection\\test_images\Curved.png')) # Read the test img
+
 warped_img = cv2.warpPerspective(img, M, (IMAGE_W, IMAGE_H)) # Image warping
 plt.imshow(cv2.cvtColor(warped_img, cv2.COLOR_BGR2RGB)) # Show results
 plt.show()
+cv2.imwrite('./BEV.png', cv2.cvtColor(warped_img, cv2.COLOR_RGB2BGR))
